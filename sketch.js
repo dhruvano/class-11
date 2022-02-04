95018c6a0148a13fb1c28d92827d00711761b098
dhruvano <93755130+dhruvano@users.noreply.github.com>
2022-02-04 19:13:24 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+// for loading images 
+  function preload() {
+     trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+     trex_collided = loadImage("trex_collided.png");
+     groundImage = loadImage("ground2.png")
+  }
+
+// for setting up
+  function setup() {
+    createCanvas(600, 200);
+
+     //create a trex sprite
+    trex = createSprite(50,160,20,50);
+    trex.addAnimation("running", trex_running);
+    trex.scale = 0.5;
+     
+     //create a ground sprite
+    ground = createSprite(200,180,400,20);
+    ground.addImage("ground",groundImage);
+    ground.x = ground.width /2;
+    ground.velocityX = -4;
+
+    // create invisible ground
+      invisibleGround = createSprite(200,190,400,10)
+    invisibleGround.visible=false;
+
+   }
+
+
+// create background 
+  function draw() {
+   background(220);
+
+   console.log(trex.y);
+//jump when the space button is pressed
+    if ( keyDown("space") && trex.y >= 100 ) {
+    trex.velocityY = -10;
+    }
+    
+ // to give gravity   
+   trex.velocityY = trex.velocityY + 0.8
+
+    if (ground.x < 0) {
+     ground.x = ground.width / 2;
+   }
+   
+// stop trex from falling down
+    trex.collide(invisibleGround); 
+
+    drawSprites();
+   }
